87f2a30e6655d28af8c373ec2d656db7830cb8ee
Andressa Gonçalves <andressagoncalves1997@gmail.com>
2021-10-05 22:36:24 -0300

    Add files via upload

--- ex-estruturasequencial/lib/src/main/java/ex/estruturasequencial/CalculePesoIdeal.java
+++ ex-estruturasequencial/lib/src/main/java/ex/estruturasequencial/CalculePesoIdeal.java
@@ -17,10 +17,10 @@ public class CalculePesoIdeal {
 		double peso_ideal;
 		
 		if(HouM == 1) {
-			peso_ideal = (72.7*altura) - 58;
+			peso_ideal = (72.7 * altura) - 58;
 			JOptionPane.showMessageDialog(null, "O seu peso ideal e: " + peso_ideal);
 		} else {
-			peso_ideal = (62.1*altura) - 44.7;
+			peso_ideal = (62.1 * altura) - 44.7;
 			JOptionPane.showMessageDialog(null, "O seu peso ideal e: " + peso_ideal);
 		}
 	}
