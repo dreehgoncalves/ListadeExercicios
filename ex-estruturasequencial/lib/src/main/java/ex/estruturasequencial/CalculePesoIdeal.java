c3d05070ee160d8f5cac29008523afe623a5b921
Andressa Gonçalves <andressagoncalves1997@gmail.com>
2021-10-06 15:26:20 -0300

    Update CalculePesoIdeal.java

--- ex-estruturasequencial/lib/src/main/java/ex/estruturasequencial/CalculePesoIdeal.java
+++ ex-estruturasequencial/lib/src/main/java/ex/estruturasequencial/CalculePesoIdeal.java
@@ -7,16 +7,16 @@ import javax.swing.JOptionPane;
 public class CalculePesoIdeal {
 
 	public static void main(String[] args) {
-		//Exercicios 12 e 13
+		// Exercicios 12 e 13
 		String alt = JOptionPane.showInputDialog("Informe a altura:");
 		double altura = Double.parseDouble(alt);
-		
-		String i  = JOptionPane.showInputDialog("Digite qual opção desejada:\n 1-Homem 2-Mulher");
+
+		String i = JOptionPane.showInputDialog("Digite qual opcao desejada:\n 1-Homem 2-Mulher");
 		int HouM = Integer.parseInt(i);
-		
+
 		double peso_ideal;
-		
-		if(HouM == 1) {
+
+		if (HouM == 1) {
 			peso_ideal = (72.7 * altura) - 58;
 			JOptionPane.showMessageDialog(null, String.format("O seu peso ideal e: %.2f kg", peso_ideal));
 		} else {
