b466d1eef67d1fdcdd61f74be4aab16b7397db0f
Andressa Gonçalves <andressagoncalves1997@gmail.com>
2021-10-13 13:08:12 -0300

    Add files via upload

--- ex-listadeexercicios/lib/src/main/java/ex/estruturadedecisao/ExibirTri.java
+++ ex-listadeexercicios/lib/src/main/java/ex/estruturadedecisao/ExibirTri.java
@@ -14,15 +14,22 @@ public class ExibirTri {
         String l3 = JOptionPane.showInputDialog("Informe a medida do primeiro lado:");
         double lado3 = Double.parseDouble(l3);
 
-        double soma = lado1 + lado2;
+        double soma1 = lado1 + lado2;
+        double soma2 = lado1 + lado3;
+        double soma3 = lado3 + lado2;
 
-        if (soma > lado3) {
-            if (lado1 == lado2 && lado1 == lado3) {
-                JOptionPane.showMessageDialog(null, "Essas medidas formam um triângulo equilatero");
-            } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
-                JOptionPane.showMessageDialog(null, "Essas medidas formam um triângulo isoceles");
-            } else if (lado1 != lado2 && lado2 != lado3) {
-                JOptionPane.showMessageDialog(null, "Essas medidas formam um triângulo escaleno");
+        if (lado1 != 0 && lado2 != 0 && lado3 != 0) {
+
+            if (soma1 > lado3 || soma2 > lado2 || soma3 > lado1) {
+
+                if (lado1 == lado2 && lado1 == lado3) {
+                    JOptionPane.showMessageDialog(null, "Essas medidas formam um triângulo equilatero");
+                } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+                    JOptionPane.showMessageDialog(null, "Essas medidas formam um triângulo isoceles");
+                } else if (lado1 != lado2 && lado2 != lado3) {
+                    JOptionPane.showMessageDialog(null, "Essas medidas formam um triângulo escaleno");
+                }
+
             }
         } else {
             JOptionPane.showMessageDialog(null, "Essas medidas não formam um triângulo");
