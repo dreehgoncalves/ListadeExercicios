b9b4eb07d01bea8506ca85736b1a5db3461389a7
Andressa Gonçalves <andressagoncalves1997@gmail.com>
2021-10-15 15:03:44 -0300

    Add files via upload

--- ex-listadeexercicios/lib/src/main/java/ex/estruturaderepeticao/ExibirNumEntre.java
+++ ex-listadeexercicios/lib/src/main/java/ex/estruturaderepeticao/ExibirNumEntre.java
@@ -11,9 +11,15 @@ public class ExibirNumEntre {
 
         String n2 = JOptionPane.showInputDialog("Informe um numero:");
         int num2 = Integer.parseInt(n2);
+        int soma = 0;
 
         for (int inicio = num1 + (1); inicio < num2; inicio++) {
-            System.out.printf("%d\n", inicio);
+            // soma ex - 11
+            soma += inicio;
+
+            // ex - 10
+            // System.out.printf("%d\n", inicio);
         }
+        System.out.printf("%d\n", soma);
     }
 }
