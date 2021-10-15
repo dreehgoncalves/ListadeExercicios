e99c6e444f3f50e87a6b96e23de1a1bc550f351e
Andressa Gonçalves <andressagoncalves1997@gmail.com>
2021-10-15 14:29:29 -0300

    Add files via upload

--- ex-listadeexercicios/lib/src/main/java/ex/estruturaderepeticao/ExibirNumEntre.java
+++ ex-listadeexercicios/lib/src/main/java/ex/estruturaderepeticao/ExibirNumEntre.java
@@ -1,14 +1,13 @@
 package ex.estruturaderepeticao;
 
-public class ExibirNumImpar {
-    public static void main(String[] args) {
+public class ExibirNumEntre {
 
+    public static void main(String[] args) {
         for (int a = 1; a <= 50; a++) {
 
             if (a % 2 == 1) {
                 System.out.printf("%d\n", a);
             }
         }
-
     }
 }
